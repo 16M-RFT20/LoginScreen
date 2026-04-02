5ec5b0671af85c2c5e710427a22086d92eb7f586
kim dg <dgkim729@gmail.com>
2026-04-02 10:48:13 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("한컴 고딕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppName.Location = new Point(168, 43);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(186, 78);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtID.Location = new Point(75, 147);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(369, 51);
+            txtID.TabIndex = 1;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtPW.Location = new Point(75, 216);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(369, 51);
+            txtPW.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(192, 255, 255);
+            btnLogin.Font = new Font("한컴 고딕", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnLogin.ForeColor = Color.Blue;
+            btnLogin.Location = new Point(186, 300);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(142, 64);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(516, 401);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
+    }
+}
